e5019e3780e9d679ea438c1d0a7d42861ad4ccc4
LeTrongKhiem <19130102@st.hcmuaf.edu.vn>
2022-12-31 19:48:09 +0700

    refactor and change algorithm RSA

--- src/main/java/com/example/webproject/service/Signature/AlgorithmRSA.java
+++ src/main/java/com/example/webproject/service/Signature/AlgorithmRSA.java
@@ -6,85 +6,92 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 
 public class AlgorithmRSA {
-    private BigInteger a, b, c;
+    private BigInteger n, decrypt, encrypt;
 
     public AlgorithmRSA() {
     }
 
-    public AlgorithmRSA(BigInteger a, BigInteger b, BigInteger c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public AlgorithmRSA(BigInteger n, BigInteger decrypt, BigInteger encrypt) {
+        this.n = n;
+        this.decrypt = decrypt;
+        this.encrypt = encrypt;
     }
 
     public AlgorithmRSA(BigInteger newn, BigInteger newe) {
-        a = newn;
-        c = newe;
+        n = newn;
+        encrypt = newe;
     }
 
-    public BigInteger getA() {
-        return a;
+    public BigInteger getN() {
+        return n;
     }
 
-    public void setA(BigInteger a) {
-        this.a = a;
+    public void setN(BigInteger n) {
+        this.n = n;
     }
 
-    public BigInteger getB() {
-        return b;
+    public BigInteger getDecrypt() {
+        return decrypt;
     }
 
-    public void setB(BigInteger b) {
-        this.b = b;
+    public void setDecrypt(BigInteger decrypt) {
+        this.decrypt = decrypt;
     }
 
-    public BigInteger getC() {
-        return c;
+    public BigInteger getEncrypt() {
+        return encrypt;
     }
 
-    public void setC(BigInteger c) {
-        this.c = c;
+    public void setEncrypt(BigInteger encrypt) {
+        this.encrypt = encrypt;
     }
 
-    public BigInteger encrypt(BigInteger m) {
-        return m.modPow(b, a);
-    }
-
+    /**
+     * Encrypt the given plaintext message.
+     */
     public synchronized String encrypt(String message) {
-        return (new BigInteger(message.getBytes())).modPow(b, a).toString();
+        return (new BigInteger(message.getBytes())).modPow(decrypt, n).toString();
     }
 
-    public BigInteger decrypt(BigInteger m) {
-        return m.modPow(c, a);
+    /**
+     * Encrypt the given plaintext message.
+     */
+    public synchronized BigInteger encrypt(BigInteger message) {
+        return message.modPow(decrypt, n);
     }
 
+    /**
+     * Decrypt the given ciphertext message.
+     */
     public synchronized String decrypt(String message) {
-        return new String((new BigInteger(message)).modPow(c, a).toByteArray());
+        return new String((new BigInteger(message)).modPow(encrypt, n).toByteArray());
     }
 
-    public BigInteger sign(BigInteger m) {
-        return m.modPow(b, c);
+    /**
+     * Decrypt the given ciphertext message.
+     */
+    public synchronized BigInteger decrypt(BigInteger message) {
+        return message.modPow(encrypt, n);
     }
 
-    public synchronized String sign(String message) {
-        return (new BigInteger(message.getBytes())).modPow(b, a).toString();
-    }
+
 
     public void KeyRSA(int bits) {
         SecureRandom r = new SecureRandom();
         BigInteger p = new BigInteger(bits / 2, 100, r);
         BigInteger q = new BigInteger(bits / 2, 100, r);
-        a = p.multiply(q);
+        n = p.multiply(q);
         BigInteger m = (p.subtract(BigInteger.ONE)).multiply(q
                 .subtract(BigInteger.ONE));
         boolean found = false;
         do {
-            c = new BigInteger(bits / 2, 50, r);
-            if (m.gcd(c).equals(BigInteger.ONE) && c.compareTo(m) < 0) {
+            encrypt = new BigInteger(bits / 2, 50, r);
+            if (m.gcd(encrypt).equals(BigInteger.ONE) && encrypt.compareTo(m) < 0) {
                 found = true;
             }
         } while (!found);
-        b = c.modInverse(m);
+        decrypt = encrypt.modInverse(m);
+        System.out.println("n = " + n);
     }
 
     public static void main(String[] args) throws Exception {
@@ -95,14 +102,14 @@ public class AlgorithmRSA {
 
         String bi = new String(sha1.getSHA1("D:\\LapTrinhWeb\\JSPDemo\\WebProject\\src\\main\\java\\com\\example\\webproject\\Authentication\\show.png").toString());
         BigInteger text1 = new BigInteger(bi);
-        System.out.println("Plaintext: " + text1);
-//  BigInteger plaintext = new BigInteger(text1);
-
-        BigInteger ciphertext = rsa.encrypt(text1);
-        System.out.println("Ciphertext: " + ciphertext);
-//  plaintext = rsa.decrypt(ciphertext);
-
-        BigInteger text2 = rsa.decrypt(ciphertext);
-        System.out.println("Plaintext: " + text2);
+//        System.out.println("Plaintext: " + text1);
+////  BigInteger plaintext = new BigInteger(text1);
+//
+//        BigInteger ciphertext = rsa.encrypt(text1);
+//        System.out.println("Ciphertext: " + ciphertext);
+////  plaintext = rsa.decrypt(ciphertext);
+//
+//        BigInteger text2 = rsa.decrypt(ciphertext);
+//        System.out.println("Plaintext: " + text2);
     }
 }
